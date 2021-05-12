bf994e58b3f80ea77af55e4076cecb256162881e
Haomiao Yu <60568423+Samshui@users.noreply.github.com>
2021-05-12 09:08:52 +0800

    修改

--- src/main/java/com/example/myspring/Application.java
+++ src/main/java/com/example/myspring/Application.java
@@ -1,10 +1,8 @@
 package com.example.myspring;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.example.myspring.mapper")
